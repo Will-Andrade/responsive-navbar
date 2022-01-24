d13c230c2395b991cb51f67a7da4a5e181a7c7ca
Will-Andrade <willian.santos.andra@outlook.com>
2022-01-24 13:27:41 -0400

    adds the click event for the hamburguer

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+const togglebtn = document.querySelector('[data-js="toggle-btn"]');
+const navbarLinks = document.querySelector('[data-js="navbar-links"]');
+
+togglebtn.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+})
